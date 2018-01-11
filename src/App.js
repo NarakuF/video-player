fdc984700aee9d574b530c7fec98b884c406de07
NarakuF <lgd9598@gmail.com>
2018-01-11 11:08:12 -0500

    play video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import VideoPlayer from './VideoPlayer';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        const videoJsOptions = {
+            autoplay: true,
+            controls: true,
+            sources: [{
+                src: '//vjs.zencdn.net/v/oceans.mp4',
+                type: 'video/mp4'
+            }, {
+                src: '//vjs.zencdn.net/v/oceans.webm',
+                type: 'video/webm'
+            }, {
+                src: '//vjs.zencdn.net/v/oceans.ogv',
+                type: 'video/ogg'
+            }]
+        };
+        return <VideoPlayer {...videoJsOptions} />;
+    }
 }
 
 export default App;
