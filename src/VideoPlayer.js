00ba4fc86b37a0b5f3239ee9306937bff3a67ca2
NarakuF <lgd9598@gmail.com>
2018-01-11 15:05:05 -0500

    change videos

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -2,6 +2,11 @@ import React from 'react';
 import videojs from 'video.js';
 
 export default class VideoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSourceChange = this.handleSourceChange.bind(this);
+    }
+
     componentDidMount() {
         // instantiate Video.js
         this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
@@ -16,10 +21,21 @@ export default class VideoPlayer extends React.Component {
         }
     }
 
-   render() {
+    handleSourceChange(event) {
+        event.preventDefault();
+        this.player.src({src: event.target.elements.source.value});
+    }
+
+    render() {
         return (
-            <div data-vjs-player>
-                <video ref={ node => this.videoNode = node } className="video-js"></video>
+            <div className="row">
+                <form className="form-inline col-md-12 my-3" onSubmit={this.handleSourceChange}>
+                    <input type="text" className="form-control w-75 mr-auto" name="source"/>
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </form>
+                <div data-vjs-player className="col-md-12">
+                    <video ref={node => this.videoNode = node} className="video-js" width="800" height="450"></video>
+                </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import VideoPlayer from './VideoPlayer';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [{
+        src: 'http://techslides.com/demos/samples/sample.mp4'
+    }]
+};
+
+ReactDOM.render(<VideoPlayer {...videoJsOptions} />, document.getElementById('root'));
+
+//vjs.zencdn.net/v/oceans.mp4
