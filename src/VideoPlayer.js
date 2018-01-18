b9fddd2291a6c2e2d9f7856886c620d197d3e74a
NarakuF <lgd9598@gmail.com>
2018-01-18 15:42:00 -0500

    desc

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import videojs from 'video.js';
+// eslint-disable-next-line
 import videojsYoutube from 'videojs-youtube';
+// eslint-disable-next-line
 import videojsMarker from 'videojs-markers';
+import ContentEditable from 'react-contenteditable'
 import './videojs.markers.css';
 
 export default class VideoPlayer extends React.Component {
@@ -10,9 +13,13 @@ export default class VideoPlayer extends React.Component {
         this.state = {
             like: false,
             likes: [],
+            description: "",
         };
         this.handleSourceChange = this.handleSourceChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleLikeDescriptionChange = this.handleLikeDescriptionChange.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
     componentDidMount() {
@@ -44,17 +51,44 @@ export default class VideoPlayer extends React.Component {
         if (!this.state.like) {
             this.player.markers.add([{
                 time: this.player.currentTime(),
-                text: "Like " + (markers.length + 1),
+                text: `Like: ${this.player.currentTime().toFixed(2)}s`,
             }]);
         }
         else {
             markers[markers.length - 1].duration = this.player.currentTime() -
                 markers[markers.length - 1].time;
             this.player.markers.updateTime();
-            this.setState({likes: markers})
         }
-        this.setState(prevState => ({like: !prevState.like}));
-        console.log(this.state.likes);
+        this.setState(prevState => ({like: !prevState.like, likes: markers}));
+        console.log(markers)
+    }
+
+    handleDescriptionChange(event) {
+        event.preventDefault();
+        this.setState({description: event.target.value});
+    }
+
+    handleLikeDescriptionChange(event, like) {
+        let markers = this.player.markers.getMarkers();
+        markers.map(m => {
+            if (m.key === like.key) {
+                m.text = event.target.value;
+            }
+        });
+        this.setState({likes: markers});
+    }
+
+    handleRemove(like) {
+        let markers = this.player.markers.getMarkers();
+        let i;
+        for (i = 0; i < markers.length; i++) {
+            if (markers[i].key === like.key) {
+                this.player.markers.remove([i]);
+                break;
+            }
+        }
+        this.setState({likes: markers});
+        console.log(markers)
     }
 
     render() {
@@ -73,9 +107,13 @@ export default class VideoPlayer extends React.Component {
                         {this.state.like ? 'Unlike' : 'Like'}
                     </button>
                 </div>
-                <div className="col-md-10">
+                <div className="col-md-10 my-4">
                     <h4>Description</h4>
-                    <div className="mb-4" contentEditable><textarea className="form-control" rows="3" placeholder="This is about ..."></textarea></div>
+                    <textarea className="form-control" rows="3"
+                              placeholder={"This is about ..." + this.state.description}
+                              onChange={this.handleDescriptionChange}></textarea>
+                </div>
+                <div className="col-md-10">
                     <h4>Likes</h4>
                     <table className="table table-striped table-bordered">
                         <thead>
@@ -83,15 +121,26 @@ export default class VideoPlayer extends React.Component {
                             <th scope="col">From (Second)</th>
                             <th scope="col">To (Second)</th>
                             <th scope="col">Description</th>
+                            <th></th>
                         </tr>
                         </thead>
-                        <tbody>{this.state.likes.map(like => {
-                            return (<tr key={like.key}>
-                                <td>{like.time.toFixed(2)}</td>
-                                <td>{(like.time + like.duration).toFixed(2)}</td>
-                                <td><div contentEditable onInput={() => 1}>{like.text}</div></td>
-                            </tr>)
-                        })}</tbody>
+                        <tbody>
+                        {this.state.likes.map(like => {
+                            return (
+                                <tr key={like.key}>
+                                    <td>{like.time.toFixed(2)}</td>
+                                    <td>{(like.time + like.duration).toFixed(2)}</td>
+                                    <td><ContentEditable html={like.text}
+                                                         onChange={e => this.handleLikeDescriptionChange(e, like)}/>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-sm btn-outline-danger"
+                                                onClick={e => this.handleRemove(like)}>Remove
+                                        </button>
+                                    </td>
+                                </tr>)
+                        })}
+                        </tbody>
                     </table>
                 </div>
             </div>
