099ede12db0028105b0d6e03919fac2fbe757843
NarakuF <lgd9598@gmail.com>
2018-03-01 12:10:36 -0500

    v3

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -15,24 +15,29 @@ export default class VideoPlayer extends React.Component {
         super(props);
         this.state = {
             record: -1,
-            rec_id: "",
+            rec_key: "",
             records: [],
             onlineCopy: [],
             description: "",
             status: Status.VIEW,
+            timeOffset: 1,
         };
         this.reset = this.reset.bind(this);
         this.getIdx = this.getIdx.bind(this);
+        this.getKey = this.getKey.bind(this);
         this.changeSrc = this.changeSrc.bind(this);
         this.clickRecord = this.clickRecord.bind(this);
         this.changeDescription = this.changeDescription.bind(this);
         this.changeRecordDescription = this.changeRecordDescription.bind(this);
         this.deleteRecord = this.deleteRecord.bind(this);
         this.playRecord = this.playRecord.bind(this);
+        this.addRecord = this.addRecord.bind(this);
         this.updateRecord = this.updateRecord.bind(this);
         this.offline = this.offline.bind(this);
         this.prev = this.prev.bind(this);
         this.next = this.next.bind(this);
+        this.changeTime = this.changeTime.bind(this);
+        this.renderOfflineTable = this.renderOfflineTable.bind(this);
         this.renderToolBar = this.renderToolBar.bind(this);
     }
 
@@ -42,10 +47,10 @@ export default class VideoPlayer extends React.Component {
             markerStyle: {
                 "width": "5px",
                 "border-radius": "0%",
-                "background-color": "red"
+                "background-color": "red",
             },
             onMarkerClick: function () {
-                return false
+                return false;
             },
         });
         this.reset();
@@ -61,24 +66,32 @@ export default class VideoPlayer extends React.Component {
         this.player.markers.reset([]);
         this.setState({
             record: -1,
-            rec_id: "",
+            rec_key: "",
             records: [],
             offlineRecords: [],
             description: "",
             status: Status.VIEW,
+            timeOffset: 1,
         });
         this.player.controlBar.progressControl.disable();
         this.player.one("ended", () => {
             this.setState({status: Status.REVIEW});
             if (this.state.records.length > 0) {
                 this.setState({description: ""});
-                this.playRecord(this.state.records[0]);
+                this.playRecord(this.getKey(0));
             }
         });
     }
 
-    getIdx(key = this.state.rec_id) {
+    getIdx(key = this.state.rec_key) {
         return this.state.records.findIndex(rec => rec.key === key);
+    }
+
+    getKey(idx) {
+        if (idx >= 0 && idx < this.state.records.length) {
+            return this.state.records[idx].key;
+        }
+        return "";
     }
 
     changeSrc(e) {
@@ -124,37 +137,59 @@ export default class VideoPlayer extends React.Component {
         this.setState({records: this.state.records});
     }
 
-    deleteRecord(rec) {
+    deleteRecord(key = this.state.rec_key) {
+        if (key !== "") {
+            const markers = this.player.markers.getMarkers();
+            const idx = markers.findIndex(m => m.key === key);
+            this.player.markers.remove([idx]);
+            const newKey = this.getKey(Math.min(idx, markers.length - 1));
+            this.playRecord(newKey);
+            this.setState({records: markers});
+        }
+    }
+
+    playRecord(key) {
+        //window.scrollTo(0,0);
+        if (key !== "") {
+            const rec = this.state.records[this.getIdx(key)];
+            this.player.play(this.player.currentTime(rec.time));
+            let getToPlay = () => {
+                if (this.player.currentTime() >= rec.time + rec.duration) {
+                    this.player.pause();
+                    this.player.off("timeupdate", getToPlay);
+                }
+            };
+            this.player.on("timeupdate", getToPlay);
+        }
+        this.setState({rec_key: key});
+    }
+
+    addRecord(){
         const markers = this.player.markers.getMarkers();
-        const idx = markers.findIndex(m => m.key === rec.key);
-        this.player.markers.remove([idx]);
+        const time = this.player.currentTime();
+        this.player.markers.add([{
+            time: time,
+            duration: 0,
+            text: "",
+        }]);
         this.setState({records: markers});
     }
 
-    playRecord(rec) {
-        this.player.play(this.player.currentTime(rec.time));
-        let getToPlay = () => {
-            if (this.player.currentTime() >= rec.time + rec.duration) {
-                this.player.pause();
-                this.player.off("timeupdate", getToPlay);
+    updateRecord(n, key = this.state.rec_key) {
+        if (key !== "") {
+            const time = this.player.currentTime();
+            let rec = this.state.records[this.getIdx(key)];
+            if (n === 1) {
+                const end = rec.time + rec.duration;
+                rec.time = time;
+                rec.duration = end - rec.time;
             }
-        };
-        this.player.on("timeupdate", getToPlay);
-        this.setState({rec_id: rec.key});
-    }
-
-    updateRecord(rec, n) {
-        const time = this.player.currentTime();
-        if (n === 1) {
-            const end = rec.time + rec.duration;
-            rec.time = time;
-            rec.duration = end - rec.time;
+            else {
+                rec.duration = time - rec.time;
+            }
+            this.player.markers.updateTime();
+            this.setState({records: this.state.records});
         }
-        else {
-            rec.duration = time - rec.time;
-        }
-        this.player.markers.updateTime();
-        this.setState({records: this.state.records});
     }
 
     offline(e) {
@@ -164,6 +199,7 @@ export default class VideoPlayer extends React.Component {
         if (flag) {
             const onlineCopy = JSON.parse(JSON.stringify(this.state.records));
             this.setState({onlineCopy: onlineCopy, status: Status.OFFLINE});
+            this.playRecord(this.getKey(0));
             this.player.controlBar.progressControl.enable();
         }
         else {
@@ -175,7 +211,7 @@ export default class VideoPlayer extends React.Component {
         e.preventDefault();
         const idx = this.getIdx();
         if (idx - 1 >= 0) {
-            this.playRecord(this.state.records[idx - 1]);
+            this.playRecord(this.getKey(idx - 1));
         }
     }
 
@@ -183,8 +219,45 @@ export default class VideoPlayer extends React.Component {
         e.preventDefault();
         const idx = this.getIdx();
         if (idx + 1 < this.state.records.length) {
-            this.playRecord(this.state.records[idx + 1]);
+            this.playRecord(this.getKey(idx + 1));
         }
+    }
+
+    changeTime(time) {
+        let curTime = this.player.currentTime();
+        this.player.currentTime(curTime + time);
+    }
+
+    renderOfflineTable() {
+        return <table className="table table-sm table-striped table-bordered text-center">
+            <thead>
+            <tr>
+                <th style={{width: "5%"}}></th>
+                <th style={{width: "10%"}}>Start</th>
+                <th style={{width: "10%"}}>End</th>
+                <th style={{width: "40%"}}>Description</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.records.map(rec => {
+                return (
+                    <tr key={rec.key}>
+                        <td>
+                            <button
+                                className={"btn btn-sm btn-outline-danger"
+                                + (this.state.rec_key === rec.key ? " active" : "")}
+                                onClick={() => this.playRecord(rec.key)}></button>
+                        </td>
+                        <td style={{width: "10%"}}>{videojs.formatTime(rec.time)}</td>
+                        <td style={{width: "10%"}}>{videojs.formatTime(rec.time + rec.duration)}</td>
+                        <td style={{width: "40%"}} className="text-justify"><ContentEditable
+                            html={rec.text}
+                            onChange={e => this.changeRecordDescription(e, rec)}/>
+                        </td>
+                    </tr>)
+            })}
+            </tbody>
+        </table>;
     }
 
     renderToolBar() {
@@ -204,7 +277,7 @@ export default class VideoPlayer extends React.Component {
                         <button className="btn btn-danger"
                                 onClick={this.next}>Next
                         </button>
-                        {this.state.records.filter(rec => rec.key === this.state.rec_id).map(rec => {
+                        {this.state.records.filter(rec => rec.key === this.state.rec_key).map(rec => {
                             return (
                                 <input key={rec.key} className="form-control w-75" type="text"
                                        placeholder="This is about ..."
@@ -224,43 +297,32 @@ export default class VideoPlayer extends React.Component {
                     </div>
                 </form>;
             case Status.OFFLINE:
-                console.log("offline");
-                return <table className="table table-striped table-bordered text-center">
-                    <thead>
-                    <tr>
-                        <th style={{width: "10%"}}>Start</th>
-                        <th style={{width: "10%"}}>End</th>
-                        <th style={{width: "40%"}}>Description</th>
-                        <th style={{width: "5%"}}></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.records.map(rec => {
-                        return (
-                            <tr key={rec.key}>
-                                <td style={{width: "10%"}}>{videojs.formatTime(rec.time)}
-                                    <button className="btn btn-sm btn-outline-danger float-right"
-                                            onClick={() => this.updateRecord(rec, 1)}>Save
-                                    </button>
-                                </td>
-                                <td style={{width: "10%"}}>{videojs.formatTime(rec.time + rec.duration)}
-                                    <button className="btn btn-sm btn-outline-danger float-right"
-                                            onClick={() => this.updateRecord(rec, 2)}>Save
-                                    </button>
-                                </td>
-                                <td style={{width: "40%"}} className="text-justify"><ContentEditable
-                                    html={rec.text}
-                                    onChange={e => this.changeRecordDescription(e, rec)}/>
-                                </td>
-                                <td style={{width: "5%"}}>
-                                    <button className="btn btn-sm btn-outline-danger"
-                                            onClick={e => this.deleteRecord(rec)}>Delete
-                                    </button>
-                                </td>
-                            </tr>)
-                    })}
-                    </tbody>
-                </table>;
+                return <div className="d-flex justify-content-around">
+                    <div className="btn-group">
+                        <button className="btn btn-outline-danger"
+                                onClick={() => this.changeTime(-this.state.timeOffset - 0.5)}>-
+                        </button>
+                        <input className="w-25 text-center"
+                               type="number"
+                               value={this.state.timeOffset}
+                               onChange={e => this.setState({timeOffset: Math.max(e.target.value, 0)})}/>
+                        <button className="btn btn-outline-danger"
+                                onClick={() => this.changeTime(this.state.timeOffset)}>+
+                        </button>
+                    </div>
+                    <button className="btn btn-outline-danger mr-3"
+                            onClick={() => this.updateRecord(1)}>Save as Start
+                    </button>
+                    <button className="btn btn-outline-danger mr-3"
+                            onClick={() => this.updateRecord(2)}>Save as End
+                    </button>
+                    <button className="btn btn-success mr-3"
+                            onClick={this.addRecord}>Add
+                    </button>
+                    <button className="btn btn-danger"
+                            onClick={() => this.deleteRecord()}>Delete
+                    </button>
+                </div>;
             default:
                 console.log(0);
                 return null;
@@ -281,23 +343,23 @@ export default class VideoPlayer extends React.Component {
                     <video ref={node => this.videoNode = node} className="video-js"></video>
                 </div>
 
-                <div id="markers_list" className="col-md-2">
+                <div id="records_list" className="col-md-2">
                     <table className="table table-sm table-striped table-bordered text-center">
-                        <thead className="mkl-h">
-                        <tr className="mkl-r">
-                            <th className="mkl-h">Markers</th>
+                        <thead className="rec-h">
+                        <tr className="rec-r">
+                            <th className="rec-h">Records</th>
                         </tr>
                         </thead>
-                        <tbody className="mkl-b">
+                        <tbody className="rec-b">
                         {this.state.records.map(rec => {
                             return (
-                                <tr className="mkl-r" key={rec.key}>
-                                    <td className="mkl-d">
+                                <tr className="rec-r" key={rec.key}>
+                                    <td className="rec-d">
                                         <button
                                             className={"btn btn-sm" + (rec.text ? " btn-success" : " btn-outline-success")
-                                            + (this.state.rec_id === rec.key ? " active" : "")}
+                                            + (this.state.rec_key === rec.key ? " active" : "")}
                                             disabled={this.state.status === Status.VIEW}
-                                            onClick={() => this.playRecord(rec)}>{videojs.formatTime(rec.time)}</button>
+                                            onClick={() => this.playRecord(rec.key)}>{videojs.formatTime(rec.time)}</button>
                                     </td>
                                 </tr>)
                         })}
@@ -308,6 +370,7 @@ export default class VideoPlayer extends React.Component {
                 <div className="col-md-10 my-4 d-flex justify-content-around">
                     {this.renderToolBar()}
                 </div>
+                {this.state.status === Status.OFFLINE && this.renderOfflineTable()}
             </div>
         )
     }
