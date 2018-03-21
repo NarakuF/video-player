d2b598e1813e9158893da34e7fb9756dca0ee174
NarakuF <lgd9598@gmail.com>
2018-03-21 18:18:19 -0400

    final

--- server/server.js
+++ server/server.js
@@ -9,19 +9,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const publicPath = path.join(__dirname, '..', 'public');
+const dataPath = path.join(__dirname, '..', 'data');
 
 app.use(Express.static(publicPath));
 app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')));
 app.post('/save', function(req, res) {
     const data = req.body;
     const fileName = data.src.includes('www.youtube.com') ? data.src.substr(data.src.indexOf('=') + 1) : data.src.replace(/[\/:*?"<>|]/g, '_');
-    fs.writeFile(`${fileName}.json`, JSON.stringify(data), err => {
+    fs.writeFile(path.join(dataPath, `${fileName}.json`), JSON.stringify(data), err => {
         if (err) {
             console.log(err);
         } else {
             res.send('Saved!');
         }
-    })
+    });
 });
 
 app.listen(8080, () => console.log('running on port 8080'));
